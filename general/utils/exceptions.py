f5f73fb2162f4dfaba98966d5b97ab86ebaae303
opulentfox-29 <=>
2023-04-12 14:52:29 +0300

    fix typo

--- general/utils/exceptions.py
+++ general/utils/exceptions.py
@@ -1,3 +1,3 @@
 class TooLargeVideo(Exception):
     def __init__(self, size: float):
-        self.text = f"[TG ERROR] Видео слишком большое ({size} MB)."
+        self.text = f"[get video] Видео слишком большое ({size} MB)."
